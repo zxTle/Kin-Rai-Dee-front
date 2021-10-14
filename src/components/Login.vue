1284b142a50b595c1ca12930972e2513a29e3f7e
waaarisra <60773471+waaarisra@users.noreply.github.com>
2021-10-14 20:20:03 +0700

    sign up add condition check null

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,26 +6,42 @@
     <div class="input field">
       <label>Email</label>
       <v-col>
-        <v-text-field></v-text-field>
+        <v-text-field v-model="email"></v-text-field>
       </v-col>
     </div>
 
     <div class="input-field">
         <label>Password</label>
-        <v-text-field></v-text-field>
+        <v-text-field v-model="password"></v-text-field>
     </div>
     </v-form>
 
 
-    <v-btn class="btn btn-large">Login</v-btn>
+    <v-btn v-on:click="login" class="btn btn-large">Login</v-btn>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
     name: 'login',
     data: function(){
-        return{}
+        return{
+          email: "",
+          password:""
+        };
+    },
+    methods:{
+      login: function(e){
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(this.email,this.password)
+          .then(
+            // user => {
+
+            // }
+          )
+      }
     }
 };
 </script>
--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -75,7 +75,9 @@
         </v-col>
       </div>
       <div class="d-flex flex-row">
-        <label class="font-FC-Palette font-size pt-5 ml-5">Confirm passsword :</label>
+        <label class="font-FC-Palette font-size pt-5 ml-5"
+          >Confirm passsword :</label
+        >
         <v-col class="filepass" md="5">
           <v-text-field
             class="text-field-size290"
@@ -124,7 +126,6 @@
 </template>
 
 <script>
-import firebase from 'firebase'
 import Account from "../store/Accounts";
 import { format, parseISO } from "date-fns";
 export default {
@@ -142,12 +143,17 @@ export default {
       account: [],
       menu: false,
       loading: false,
-      show1 : false,
-      show2 : false,
-      rule :{
-        checkcf :[value => (value == this.signup_form.password) || 'พาสเวิร์ดไม่ตรงกัน'],
-        checkmin : [value => (value && value.length >= 6) || 'ความยาวอย่างน้อย 6 ตัวอักษร']
-      }
+      show1: false,
+      show2: false,
+      rule: {
+        checkcf: [
+          (value) => value == this.signup_form.password || "พาสเวิร์ดไม่ตรงกัน",
+        ],
+        checkmin: [
+          (value) =>
+            (value && value.length >= 6) || "ความยาวอย่างน้อย 6 ตัวอักษร",
+        ],
+      },
     };
   },
   created() {
@@ -173,14 +179,15 @@ export default {
       this.$refs.menu.save(date);
     },
     addFrom() {
-      console.log(this.checkField())
-      if (!this.checkField()) {
+      console.log(this.checkField());
+
+      if (!this.checkNull()) {
+        alert("Please complete the form");
+      } else if (!this.checkField()) {
         alert("This email or username already exist");
-      }
-      else if (!this.checkOther()){
-        alert("Please check your password and confirm-password")
-      }
-      else {
+      } else if (!this.checkOther()) {
+        alert("Please check your password and confirm-password");
+      } else {
         let payload = {
           username: this.signup_form.username,
           name: this.signup_form.name,
@@ -191,31 +198,49 @@ export default {
           birthDay: this.signup_form.birthDay,
         };
         Account.dispatch("signupAccount", payload);
+        alert("create account success");
         this.clearForm();
+        this.$router.push("/");
       }
     },
     checkField() {
       this.account.forEach((acc) => {
-        console.log(acc.username)
-        console.log(this.signup_form.username)
-        if (acc.username == this.signup_form.username || acc.email == this.signup_form.email){
-          console.log("enter")
-          return false
+        console.log(acc.username);
+        console.log(this.signup_form.username);
+        if (
+          acc.username == this.signup_form.username ||
+          acc.email == this.signup_form.email
+        ) {
+          console.log("enter");
+          return false;
         }
-      })
-      return true
+      });
+      return true;
     },
-    checkOther(){
+    checkOther() {
       if (this.signup_form.password !== this.signup_form.cfpass) {
-        return false
+        return false;
+      } else if (this.signup_form.password.length < 6) {
+        return false;
+      } else {
+        return true;
       }
-      else if(this.signup_form.password.length <6 ){
-        return false
-      } 
-      else {
-        return true
+    },
+    checkNull() {
+      if (
+        this.signup_form.username === "" ||
+        this.signup_form.name === "" ||
+        this.signup_form.surName === "" ||
+        this.signup_form.email === "" ||
+        this.signup_form.password === ""||
+        this.signup_form.cfpass === "" ||
+        this.signup_form.birthDay === null
+      ) {
+        return false;
+      } else {
+        return true;
       }
-    }
+    },
   },
   computed: {
     formattedDate() {
@@ -228,10 +253,10 @@ export default {
 </script>
 
 <style scoped lang="css">
-.text-field-size290{
+.text-field-size290 {
   width: 290px;
 }
-.text-field-size300{
+.text-field-size300 {
   width: 300px;
 }
 .text-field-size {
@@ -266,7 +291,7 @@ export default {
   border: 7.75px solid #d72323;
   border-radius: 100px;
 }
-.v-text-field{
+.v-text-field {
   font-family: "supermarket";
   font-size: 20px;
 }
