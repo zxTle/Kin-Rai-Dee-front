027d2a8204431463e7d37c5a0d8467cee4d91467
Paramee21 <61011359+Paramee21@users.noreply.github.com>
2021-11-07 20:41:48 +0700

    click and can link to food page but not same name

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,49 +1,61 @@
 <template>
   
   <div class="recommend_card">
+
       <v-container class = my-3>
-         <v-row no-gutters >
-        <template v-for = "(foodData,idex) in 10">
+        <v-row no-gutters >
+          <template v-for = "(foodData,idex) in 10">
           {{picker()}}
-          <v-col md="6" :key = "idex" >
-           
-      <v-card 
-        class="my-8 recommend"
-        color= "white"
-        height="300"
-        width="400"
-        rounded-xl
-        elevation = "10"
-        to = "/food" 
-      >
-      <img 
-        :src="chosenFood_img"
-        height="200px"
-        class = "my-5 img"
-      /> 
-      <v-card-text>
-         <h2 class="food-name">{{chosenFood_name}}</h2>
-      </v-card-text>
-      </v-card>
-       </v-col>
-      </template>
+          <v-col md="6" :key = "idex">
+
+            <v-card 
+            class="my-8 recommend"
+            color= "white"
+            height="300"
+            width="400"
+            rounded-xl
+            elevation = "10"
+            @click="clickRec()"
+            >
+            <img 
+            :src="chosenFood_img"
+            height="200px"
+            class = "my-5 img"
+            /> 
+            <v-card-text>
+              <h2 class="food-name">{{chosenFood_name}}</h2>
+            </v-card-text>
+            </v-card>
+
+            <transition name="slide" appear v-if="showModal">
+              <div class="modal">
+                <foodview class="food" :chosen="chosenFood"></foodview>
+                <v-btn @click="showModal=false">Return to Home page</v-btn>
+              </div>
+            </transition>
+
+          </v-col>
+          </template>
        </v-row>
+
       </v-container>
-      <foodview :chosen="this.chosenFood"></foodview>
+
     </div>
 </template>
 
 <script>
 import Food from "../store/Foods";
 import foodview from '../views/Food';
+//import FoodResult from './FoodResult.vue';
 export default {
-  components: { foodview },
+  components: { foodview},
     data() {
         return {
            foodData : [],
            chosenFood : {},
            chosenFood_name: {},
            chosenFood_img: {},
+           showModal: false
         }
     },
    created(){
@@ -62,13 +74,16 @@ export default {
       this.foodData = Food.getters.foods
       console.log(this.foodData)
       },
+      clickRec(){
+        this.showModal = true
+      }
   }
   
 }
 </script>
 
 <style scoped lang="css">
-.reccomend_card {
+.recommend_card {
   margin: auto;
   padding-top: 40px;
   align-items: center;
@@ -84,5 +99,21 @@ export default {
 }
 .img {
   border-radius: 30px;
+}
+.modal{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  background-color: white;
+  
+}
+.food{
+  transform: scale(100%);
 }
 </style>
